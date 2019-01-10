d58e5faeb0bbc2485eb61755ae5f8992c14f27cf
pjhernandez18 <pjhernan@usc.edu>
2019-01-09 17:53:19 -0800

    got yelp results to display to page

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var path = require('path');
 var bodyParser = require('body-parser');
 var yelp = require('yelp-fusion');
+var ejsLint = require('ejs-lint');
+
 
 //Create yelp client with API key
 var client = yelp.client('ezVut3lGqFJ7HXBTot9_Y8Nv2lcAwbVucOjwhk-g2iAwiyG5gnZ0NBm9U0D7X5bxAhRI4RhJ9DHVfg37r_32BRvg2Ys0jHSZvPoW5IymJq3DyPI9bVzz8Oa3lEkxXHYx');
@@ -12,12 +15,17 @@ var port = process.env.PORT || 8080
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+
 // allow static variables to live
 app.use(express.static(__dirname));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
 //routes 
 app.get("/", function(req, res) {
-	res.render("index");
+	res.render('index');
 })
 
 app.post('/suggest', urlencodedParser, function(req, res){
@@ -26,12 +34,18 @@ app.post('/suggest', urlencodedParser, function(req, res){
 	  term: req.body.term,
 	  location: req.body.location
 	}).then(response => {
-	  console.log(response.jsonBody.businesses);
+	  console.log(response.jsonBody);
+	  // store json result in local variable
+	  var results = response.jsonBody;
+ 	  res.render('results', {data: response.jsonBody});
+
 	}).catch(e => {
 	  console.log(e);
 	});
+	// render results page with yelp data
 });
 
 app.listen(port, function() {
 	console.log("app running");
 })
+
