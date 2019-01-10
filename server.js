1bb950ed70465aef9bb02bb8286dc8f6152b2e08
pjhernandez18 <pjhernan@usc.edu>
2019-01-09 16:51:28 -0800

    able to finally get a yelp object

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var yelp = require('yelp-fusion');
+
+//Create yelp client with API key
+var client = yelp.client('ezVut3lGqFJ7HXBTot9_Y8Nv2lcAwbVucOjwhk-g2iAwiyG5gnZ0NBm9U0D7X5bxAhRI4RhJ9DHVfg37r_32BRvg2Ys0jHSZvPoW5IymJq3DyPI9bVzz8Oa3lEkxXHYx');
+
+var app = express(); 
+
+// Set port
+var port = process.env.PORT || 8080
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// allow static variables to live
+app.use(express.static(__dirname));
+
+//routes 
+app.get("/", function(req, res) {
+	res.render("index");
+})
+
+app.post('/suggest', urlencodedParser, function(req, res){
+	// yelp search query
+	 client.search({
+	  term: req.body.term,
+	  location: req.body.location
+	}).then(response => {
+	  console.log(response.jsonBody.businesses);
+	}).catch(e => {
+	  console.log(e);
+	});
+});
+
+app.listen(port, function() {
+	console.log("app running");
+})
